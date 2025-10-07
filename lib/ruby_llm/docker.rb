05e5ff525766f69fe812d4722501be65c1188324
Aaron F Stanton <afstanton@gmail.com>
2025-10-07 16:22:50 -0400

    Zeitwerk

--- lib/ruby_llm/docker.rb
+++ lib/ruby_llm/docker.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'ruby_llm'
+require 'zeitwerk'
+
+loader = Zeitwerk::Loader.for_gem_extension(RubyLLM)
+
+loader.setup
+
 require_relative 'docker/version'
 
 module RubyLlm
