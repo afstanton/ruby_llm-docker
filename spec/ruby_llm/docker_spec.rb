32f7bba75c08ffeefc93bb484db2cc39b435b5de
Aaron F Stanton <afstanton@gmail.com>
2025-10-07 16:05:13 -0400

    Initial commit

---
+++ spec/ruby_llm/docker_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyLlm::Docker do
+  it "has a version number" do
+    expect(RubyLlm::Docker::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
